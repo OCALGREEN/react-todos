edeb592c73e1b14424dfef1956838a3a3711a055
Oger Carrillo <ogercarrillo@gmail.com>
2023-04-05 13:18:58 -0700

    added bootstrap and a table

--- src/App.js
+++ src/App.js
@@ -3,21 +3,35 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="mt-5 container">
+      <dvi className="card">
+        <div className="card-header">
+          Your Todo's
+        </div>
+        <div className="card-body">
+          <table className="table table-hover">
+            <thead>
+              <tr>
+                <th scope="col">#</th>
+                <th scope="col">Description</th>
+                <th scope="col">Assiged</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th scope="row">1</th>
+                <td>Feed dog</td>
+                <td>Eric</td>
+              </tr>
+              <tr>
+                <th scope="row">2</th>
+                <td>Get haircut</td>
+                <td>Eric</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </dvi>
     </div>
   );
 }
