edc7f7e9f6967963fab506b0f5b1c0186eef6f1c
Oger Carrillo <ogercarrillo@gmail.com>
2023-04-06 16:03:30 -0700

    added delete function

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import React, {useState} from 'react'; 
 import TodoTable from './components/TodoTable';
+import NewTodoForm from './components/NewTodoForm';
 
 function App() {
 
@@ -11,30 +12,52 @@ function App() {
     {rowNumber: 4, rowDescription: "Charge phone battery", rowAssigned: "User One"}
   ]) 
 
-  const addTodo = () => {
+  const addTodo = (description, assigned) => {
+    // this will start the unique identifire for each todo 
+    let rowNumber = 0; 
+    // if there are todos in the list then do this 
     if(todos.length > 0) {
-      const newTodo = {
-        rowNumber: todos.length + 1, 
-        rowDescription: "New Todo", 
-        rowAssigned: "User Three"
-      }; 
-      setTodos(todos => [...todos, newTodo])
+      // find the last number of the element and adds 1
+      rowNumber = todos[todos.length - 1].rowNumber + 1; 
     } 
+    // else start the new row at 1
+    else {
+      rowNumber = 1; 
+    }
+    // this will create a new todo 
+      const newTodo = {
+        rowNumber: rowNumber, 
+        rowDescription: description, 
+        rowAssigned: assigned 
+      }; 
+      // will add all the old todos and add the new one 
+      setTodos(todos => [...todos, newTodo])
+  }
+
+  const deleteTodo = (deleteTodoRowNumber) => {
+    // passes in the row number of the row to be deleted 
+    // filtered will filter the row number out 
+    let filtered = todos.filter(function(value) {
+      return value.rowNumber !== deleteTodoRowNumber; 
+    })
+    // will set the new todo list 
+    setTodos(filtered); 
   }
 
   return (
     <div className="mt-5 container">
-      <dvi className="card">
+      <div className="card">
         <div className="card-header">
           Your Todo's
         </div>
         <div className="card-body">
-          <TodoTable todos={todos}/>
-          <button className="btn btn-primary" onClick={addTodo}>
+          <TodoTable todos={todos} deleteTodo={deleteTodo}/>
+          <button className="btn btn-primary">
             Add new todo
           </button>
+          <NewTodoForm addTodo = {addTodo}/>
         </div>
-      </dvi>
+      </div>
     </div>
   );
 }
